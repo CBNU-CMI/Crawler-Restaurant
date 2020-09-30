6bf68afaca0606aaa138d97e90117e98c0b2d8d3
nohgijin <ynda11111@naver.com>
2020-09-30 16:44:10 +0900

    [#1] feature: 기숙사 식당 크롤링 및 디비 설정

---
+++ yangseong.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+
+// get the client
+const mysql = require("mysql2");
+const config = require("./config.json");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+});
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://dorm.chungbuk.ac.kr/home/sub.php?menukey=20041&type=2"
+  );
+  var data = await page.evaluate(() => {
+    getData: function getData() {
+      let data = [];
+      const foodList = document.querySelector("table").querySelectorAll("tr");
+      for (let i = 1; i < foodList.length; i++) {
+        let timeList = foodList[i].querySelectorAll("td");
+        for (let j = 1; j < timeList.length; j++) {
+          data.push({
+            restaurant_name: "양성재",
+            food_name: timeList[j].innerText,
+            day: timeList[0].innerText[0],
+            date: timeList[0].innerText.split("\n")[1],
+            time: j % 4,
+          });
+        }
+      }
+      return data;
+    }
+    return getData();
+  });
+
+  data.map((d) => {
+    const query = connection.query("INSERT INTO restaurant SET ?", d, function (
+      err,
+      result
+    ) {
+      console.log(result);
+    });
+  });
+  await browser.close();
+})();
